b4980b2d2ffb6c4901b074de84dd5066f9bdb8db
John Moore <john@ncub8.com>
2018-03-02 16:48:19 -0500

    even more shit

--- marketingbs/bsphrases.js
+++ marketingbs/bsphrases.js
@@ -1,6 +1,6 @@
 var phrases = {
-    pl1: ["Maximum", "Exploding with", "Customize your", "A new paradigm in", "New", "The leader in", "All natural", "Quality", "Intense", "Feel the", "Unleash your", "American made", "Artisinal"],
-    pl2: ["quality", "power", "wholesome", "gluten-free", "organic", "faux", "simulated", "healthy", "active", "soy"],
-    pl3: ["beverage", "brand-name", "fashion", "wholesale", "flavor", "fitness", "vitamin-infused", "love-infused", "doctor-recommended", "home-made", "family-sized", "fun-sized"],
+    pl1: ["Maximum", "Exploding with", "Customize your", "A new paradigm in", "New", "The leader in", "All-natural", "Quality", "Intense", "Feel the", "Unleash your", "American made", "Artisinal", "Imported", "Hand-crafted"],
+    pl2: ["quality", "power", "wholesome", "gluten-free", "organic", "faux", "simulated", "healthy", "active", "soy", "homemade"],
+    pl3: ["beverage", "brand-name", "fashion", "wholesale", "flavor", "fitness", "vitamin-infused", "love-infused", "doctor-recommended", "home-made", "family-sized", "fun-sized", "super-sized"],
     pl4: ["customization.", "goodness.", "crystals.", "maximization.", "value.", "service.", "beauty.", "pieces.", "health."]
 }
