60f1c4261688be5473c55ba640765c26233b0b67
John Moore <john@ncub8.com>
2018-03-04 16:45:32 -0500

    more shite

--- marketingbs/bsphrases.js
+++ marketingbs/bsphrases.js
@@ -1,6 +1,6 @@
 var phrases = {
     pl1: ["Maximum", "Groundbreaking", "Exploding with", "Customize your", "A new paradigm in", "New", "The leader in", "All-natural", "High-quality", "Intense", "Feel the", "Unleash your", "American made", "Artisinal", "Imported", "Hand-crafted"],
-    pl2: ["quality", "power", "wholesome", "gluten-free", "organic", "faux", "simulated", "healthy", "active", "soy", "revolutionary", "enhanced"],
-    pl3: ["beverage", "brand-name", "fashion", "wholesale", "flavor", "fitness", "vitamin-infused", "love-infused", "doctor-recommended", "home-made", "family-sized", "fun-sized", "super-sized"],
-    pl4: ["customization.", "goodness.", "crystals.", "maximization.", "value.", "service.", "beauty.", "pieces.", "health.", "technology"]
+    pl2: ["quality", "power", "wholesome", "gluten-free", "organic", "faux", "simulated", "healthy", "active", "soy", "revolutionary", "enhanced", "pulse-pounding", "energized", "fortified", "improved"],
+    pl3: ["beverage", "brand-name", "fashion", "wholesale", "flavor", "fitness", "vitamin-infused", "love-infused", "doctor-recommended", "home-made", "family-sized", "fun-sized", "super-sized", "personalized"],
+    pl4: ["customization.", "goodness.", "crystals.", "maximization.", "value.", "service.", "beauty.", "pieces.", "health.", "technology.", "deliciousness.", "experience.", "comfort.", "power."]
 }
